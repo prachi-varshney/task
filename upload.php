2bfd7e349910b6caa6d2797eee7f2f0be0f2bf8f
Prachi Varshney <86619102+prachi-varshney@users.noreply.github.com>
2025-01-07 17:58:21 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -6,3 +6,36 @@ include "db.php";
 $emp_name=$_POST['emp_name'];
 $
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   USE test;  
+ 
+CREATE TABLE Emp (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    PhoneNo INT(10),
+    DOB DATE,
+    Gender ENUM('Male', 'Female', 'Other'),
+    Language ENUM('Hindi', 'English', 'Other'),
+    Address TEXT,
+    State VARCHAR(50),
+    District VARCHAR(50),
+    Pincode INT,
+    Experience INT,
+    Salary DECIMAL(10, 2),
+    Bio TEXT,
+    Image VARCHAR(255)
+);
